542f1e3030d82d5885d22d58e48830ecf07995e7
hasan <hassanzohdy@gmail.com>
2019-09-24 16:52:48 +0200

    Added Console Command macro

---
+++ src/Macros/Console/Command.php
@@ -0,0 +1,31 @@
+<?php
+namespace HZ\Illuminate\Mongez\Macros\Console;
+
+class Command
+{
+    /**
+     * Check if the argument exists and has a value.  
+     * 
+     * @param string $argument
+     * @return bool
+     */
+    public function argumentHasValue()
+    { 
+        return function (string $argument): bool {
+            return $this->hasArgument($argument) && $this->argument($argument);   
+        };
+    }
+    
+    /**
+     * Check if the option exists and has a value.  
+     * 
+     * @param string $option
+     * @return bool
+     */
+    public function optionHasValue()
+    { 
+        return function (string $option): bool {
+            return $this->hasOption($option) && $this->option($option);   
+        };
+    }
+}
