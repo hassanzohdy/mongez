93ae12c85debd0003f4808b510d18913c219a628
hassanzohdy <hassanzohdy@gmail.com>
2020-10-12 14:28:05 +0200

    Version 1.5.73

--- src/Macros/Database/Query/Builder.php
+++ src/Macros/Database/Query/Builder.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace HZ\Illuminate\Mongez\Macros\Database\Query;
 
 use DB;
@@ -19,15 +20,15 @@ class Builder
             $table = $this->from ?? $this->model->getTable();
 
             $statements = DB::select("SHOW TABLE STATUS LIKE '{$table}'");
-           
-            if (! $statements) {
-                throw new Exception (sprintf('Base table "%s" does not exist', $table));
+
+            if (!$statements) {
+                throw new Exception(sprintf('Base table "%s" does not exist', $table));
             }
-            
+
             return $statements[0]->Auto_increment;
         };
     }
-    
+
     /**
      * A shorthand method for the `where like ` clause
      *
@@ -43,16 +44,52 @@ class Builder
     }
 
     /**
+     * Search for location near by the given coordinates for the given distance 
+     *
+     * @example: $this->whereLocationNear('location', [20,59221, 4], 20); // search in location column for the given [lat, lng] coordinates in 20 km radius
+     * @example: $this->whereLocationNear('location', [20,59221, 4], 20, 'km'); // search in location column for the given [lat, lng] coordinates in 20 km radius
+     * @example: $this->whereLocationNear('location', [20,59221, 4], 40, 'miles'); // search in location column for the given [lat, lng] coordinates in 40 miles radius
+     * 
+     * @param  string $column
+     * @param  array $coordinates
+     * @param  float $distance
+     * @param  string $distanceType
+     * @return $this
+     */
+    public function whereLocationNear()
+    {
+        return function (string $column, array $coordinates, float $distance, string $distanceType = 'km') {
+            $distanceInRadian = $distance;
+            if ($distanceType === 'km') {
+                $distanceInRadian = $distance / 6371;
+            } elseif ($distanceType === 'miles') {
+                $distanceInRadian = $distance / 3959;
+            }
+
+            // as coordinates are based in [lat, lng] structure
+            // we need to swap the values to be [lng, lat] 
+            // @see https://docs.mongodb.com/manual/reference/operator/query/centerSphere/#op._S_centerSphere
+            $lngLatCoordinates = [$coordinates[1], $coordinates[0]];
+
+            return $this->where($column, 'geoWithin', [
+                '$geoWithin' => [
+                    '$centerSphere' => [$lngLatCoordinates, $distanceInRadian],
+                ],
+            ]);
+        };
+    }
+
+    /**
      * A shorthand method for the `or where like ` clause
      *
      * @param  string $column
      * @param  mixed $value
      * @return $this
      */
-     public function orWhereLike()
-     {
-         return function (string $column, $value) {
-             return $this->orWhere($column, 'LIKE', "%$value%");
-         };
-     }
+    public function orWhereLike()
+    {
+        return function (string $column, $value) {
+            return $this->orWhere($column, 'LIKE', "%$value%");
+        };
+    }
 }
