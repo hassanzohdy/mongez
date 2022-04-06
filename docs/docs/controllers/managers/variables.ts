da4e7a0176366df1bcb10a38bf6ddf1273a91767
Hasan Zohdy <hassanzohdy@gmail.com>
2022-04-06 16:22:25 +0200

    Adding Docs

---
+++ docs/docs/controllers/managers/variables.ts
@@ -0,0 +1,2 @@
+export const laravelMessageBag = `[Laravel MessageBag](https://laravel.com/docs/master/validation#working-with-error-messages)`;
+export const config = '[Mongez Config](./../../config/mongez)';
---
+++ docs/src/components/Highlights.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Highlights({ text }) {
+    return (
+        <span className="highlights">
+            {text}
+        </span>
+    )
+}
