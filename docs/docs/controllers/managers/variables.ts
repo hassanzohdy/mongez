95a595211a2d6325708559a0246f0f7bf62e1dae
hasan <hassanzohdy@gmail.com>
2022-05-25 17:11:25 +0200

    Enhanced Repository Request Handler And Seeder Uploads

--- docs/docs/controllers/managers/variables.ts
+++
@@ -1,2 +0,0 @@
-export const laravelMessageBag = `[Laravel MessageBag](https://laravel.com/docs/master/validation#working-with-error-messages)`;
-export const config = '[Mongez Config](./../../config/mongez)';
--- docs/src/components/Highlights.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function Highlights({ text }) {
-    return (
-        <span className="highlights">
-            {text}
-        </span>
-    )
-}
