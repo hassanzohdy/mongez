0044bec8ebea5f188be5fa3d0697c8cbdb900aea
abo3adel <abo3adel35@gmail.com>
2024-09-30 11:54:50 +0300

    feat: add model published scopes

---
+++ src/Database/Eloquent/HasPublishedState.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace HZ\Illuminate\Mongez\Database\Eloquent;
+
+use Illuminate\Database\Eloquent\Builder;
+
+trait HasPublishedState
+{
+    /**
+     * Scope a query to only include published records.
+     */
+    public function scopePublished(Builder $query, string $status = 'published'): void
+    {
+        $query->where($status, true);
+    }
+
+    /**
+     * Scope a query to exclude published records.
+     */
+    public function scopeNotPublished(Builder $query, string $status = 'published'): void
+    {
+        $query->where($status, false)->orWhereNull($status);
+    }
+
+    /**
+     * Scope a query to find a published record.
+     */
+    public function findPublished(Builder $query, int $id, string $status = 'published'): void
+    {
+        $query->where($status, true)->where('id', $id);
+    }
+}
